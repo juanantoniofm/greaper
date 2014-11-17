2fa15208d8a0e4d17d1ed05186c3fc71d0500bce
Juan Morales <juan.morales@L0147.local>
2014-11-17 17:19:41 +0000

    modifying it to parse unique uris better

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 
 
+#logfile = "access-www.siteminder.co.uk.log"
 logfile = "access-vpn.fidelio.log" # file checked for opera-myfidelio vpn
 #logfile = "access-www.channelrooms.com.log" # file to check other logs
 template_file = "email_tmplt.html"
--- test01.py
+++ test01.py
@@ -13,6 +13,36 @@ stats = {
         "200" : { "count" : 0},
         }
 
+def get_status(r=None, totalstatus=None):
+    assert r is not None
+    assert totalstatus is not None
+    try:
+        totalstatus[str(r['status'])]['count'] += 1
+    except KeyError:
+
+        totalstatus[str(r['status'])] = {}
+        totalstatus[str(r['status'])]['count'] = 1
+    return totalstatus
+
+
+def get_uri(r=None, totalstatus=None):
+    """add the request that caused the code"""
+    try:
+        totalstatus[str(r['status'])]["URI"][r['request']] += 1
+    except KeyError:
+        # if it's not being found till now, create a new entry in stats
+        try:
+            if totalstatus[str(r['status'])]["URI"]:
+                totalstatus[str(r['status'])]["URI"][r['request']] = 1
+        except:
+            totalstatus[str(r['status'])]["URI"] = {}
+            totalstatus[str(r['status'])]["URI"][r['request']] = 1
+    return totalstatus
+
+
+def get_ip(r=None, totalstatus=None):
+    pass
+
 def add_entry(r=None,totalstatus=None):
     """adds an entry to the results dictionary"""
     # TODO: this is not being GC properly, causing a memory leak.... sloppy...
@@ -22,20 +52,11 @@ def add_entry(r=None,totalstatus=None):
         #if r["status"] == 405:
             #print r
 
-        try:
-            totalstatus[str(r['status'])]['count'] += 1
-        except KeyError:
-            totalstatus[str(r['status'])] = {}
-            totalstatus[str(r['status'])]['count'] = 1
-
+        #totalstatus[str(r['status'])] = get_status(r,totalstatus)
+        totalstatus[str(r['status'])] = get_status(r,totalstatus)
 
         # add the request that caused the code
-        try:
-            totalstatus[str(r['status'])]["URI"][r['request']] += 1
-        except KeyError:
-            # if it's not being found till now, create a new entry in stats
-            totalstatus[str(r['status'])]["URI"] = {}
-            totalstatus[str(r['status'])]["URI"][r['request']] = 1
+        totalstatus[str(r['status'])]["URI"] = get_uri(r,totalstatus)
 
 ##        # add the IP count
 ##        try:
@@ -55,6 +76,8 @@ def add_entry(r=None,totalstatus=None):
     except KeyError as e:
         print("ERROR:", "some value appears to not have being registered in stats", e)
 
+    return totalstatus
+
 
 
 @consumer
@@ -62,7 +85,7 @@ def find_404():
     global stats
     while True:
         r = (yield)
-        add_entry(r, stats)
+        stats = add_entry(r, stats)
 
 
 ## 
