9221f216ab0776cda04946641bec913f297e50d5
Juan Morales <juan.morales@L0147.local>
2014-11-17 17:08:07 +0000

    moving settings to a different file

---
+++ settings.py
@@ -0,0 +1,6 @@
+
+
+logfile = "access-vpn.fidelio.log" # file checked for opera-myfidelio vpn
+#logfile = "access-www.channelrooms.com.log" # file to check other logs
+template_file = "email_tmplt.html"
+subject = """APACHE OPERA MYFIDELIO REPORT ON {{ date }}""".format("SAMPLEDATE")
