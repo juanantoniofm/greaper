982d1c83ee0388d66d5c006dfaeeea699e6cad8c
Juan Morales <juan.morales@L0147.local>
2014-11-26 14:58:46 +0000

    adding testing for helpers. messing around with mpt/mapping table

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,5 +1,55 @@
 
 import re
+import time
+
+
+def follow(thefile):
+    """
+    Follow a file like tail -f.
+    """
+    thefile.seek(0,2)
+    while True:
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+
+def consumer(func):
+    """consumer decorator and co-routine """
+    def start(*args,**kwargs):
+        c = func(*args,**kwargs)
+        c.next()
+        return c
+    return start
+
+
+def broadcast(source, consumers):
+    """
+    Broadcast a generator source to a collection of consumers
+    see
+    http://www.dabeaz.com/generators/broadcast.py
+    """
+    for item in source:
+        for c in consumers:
+            c.send(item)
+
+
+### Example of usage for broadcast
+##if __name__ == '__main__':
+##
+##    class Consumer(object):
+##        def send(self,item):
+##            print self, "got", item
+##
+##    c1 = Consumer()
+##    c2 = Consumer()
+##    c3 = Consumer()
+##
+##    from follow import *
+##    lines = follow(open("run/foo/access-log"))
+##    broadcast(lines,[c1,c2,c3])
 
 ################################################################################
 
@@ -11,15 +61,10 @@ def field_map(dictseq, name, func):
         yield d
 
 
-logpats  = r'(\S+) (\S+) (\S+) \[(.*?)\] ' \
-           r'"(\S+) (\S+) (\S+)" (\S+) (\S+) (\S+) (\S* ?\S* ?\S*)'
-           #r'"(\S+) (\S+) (\S+)" (\S+) (\S+) "(\S+)" "(\S* ?\S* ?\S*)"' # carefull, not compatible with other logs
-           #r'"(\S+) (\S+) (\S+)" (\S+) (\S+) (\S+) (\S+)'
 
-appline = r'(\w{3} \d{2} \d{2}:\d{2}:\d{2}) (app\w{4}\d{2}) ([a-z\-]*): (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}) (\w*) *\[(.*?)\] (.*?) - (.*)'
 
 #logpat   = re.compile(logpats)
-logpat = re.compile(appline)
+logpat = re.compile(mpt["channel_manager"]["regex"])
 
 def app_log(lines):
     """
@@ -56,15 +101,6 @@ def apache_log(lines):
     return log
 
 
-def consumer(func):
-    """consumer decorator and co-routine """
-    def start(*args,**kwargs):
-        c = func(*args,**kwargs)
-        c.next()
-        return c
-    return start
-
-
 def output(msg = None, level=None,output_level="DEBUG" ):
     """shows or not a message, depending on the level of output selected.
     level > level of the current message to send
@@ -81,11 +117,64 @@ def output(msg = None, level=None,output_level="DEBUG" ):
         print level, msg
         return " ".join([level,msg])
     
-import time
 
-def filter_time(strtime, out_format = "%H:%M", in_format = "%d/%b/%Y:%H:%M:%S +0000"):
-    """convert a string to a proper datetime timestamp"""
+def convert_time(strtime, in_format = "%d/%b/%Y:%H:%M:%S +0000", out_format = "%H:%M"):
+    """convert a string to a proper datetime timestamp
+        Common timestamp formats are:
+            Internet msg: "%b %d %H:%M:S"
+                "Nov 25 00:02:05",
+            ISO whatever: "%d/%b/%Y:%H:%M:%S +0000"
+                "17/Nov/2014:06:44:08 +0000",
+            log4j style : "%Y-%m-%d %H:%M:%S,%f"
+                "2014-11-25 00:02:00,180",
+    """
     stamp =  time.strptime(strtime, in_format)
     return time.strftime(out_format, stamp)
 
+
+mpt = { # a table to define differences among log formats
+        "apache": { "regex":"", r'(\S+) (\S+) (\S+) \[(.*?)\] ' \
+                           r'"(\S+) (\S+) (\S+)" (\S+) (\S+) (\S+) (\S* ?\S* ?\S*)'
+                           # carefull, not compatible with other logs:
+                           #r'"(\S+) (\S+) (\S+)" (\S+) (\S+) "(\S+)" "(\S* ?\S* ?\S*)"'
+                           #r'"(\S+) (\S+) (\S+)" (\S+) (\S+) (\S+) (\S+)'
+            "colum_names":"",
+            "funcs":""},
+        "channel_manager": { "regex": r'(\w{3} \d{2} \d{2}:\d{2}:\d{2}) ' \
+                              r'(app\w{4}\d{2}) ([a-z\-]*): ' \
+                              r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}) ' \
+                              r'(\w*) *\[(.*?)\] (.*?) - (.*)',
+            "colum_names":"",
+            "funcs":""},
+        "little_hotelier": { "regex":"",
+            "colum_names":"",
+            "funcs":""},
+        "bbapp" : {}
+        }
+
+################################################################################
+
+def guess_logkind(filename):
+    """try to guess the kind of log from the filename"""
+    raise Exception("Not f'in plemented")
+
+
+def validate_logkind(log_format, supported_formats = [ k for k in mpt.iterkeys() ]):
+    """validate the kind of log, or fail and exit with a list of supported formats"""
+    if log_format not in supported_formats:
+        raise LookupError("""I couldn't find your log kind in my list.
+        see supported: {0}""".format([f for f in mpt.iterkeys()]))
+    else:
+        return True
+
+
+def define_logkind():
+    """basic logic to find out what kind of log are we parsing"""
+    if args["log_format"] == False:
+        #- not user-defined, so try fuzzy logic
+        return validate_logkind(guess_logkind(args["input_file"]))
+    else:
+        # user defined, so check if is valid
+        return validate_logkind(args["log_format"])
+    output(logkind, "INFO",loglevel) #WAT? what the hell was i thinking with this?? TODO: delete it
 
--- myfilters.py
+++ myfilters.py
@@ -1,6 +1,13 @@
 
+################################################################################
+#   My Filters.
+#   Though as a library of possible filters to be used weth the greap tools.
+################################################################################
 def add_entry(r=None,totalstatus=None):
-    """adds an entry to the results dictionary"""
+    """
+    adds an entry to the results dictionary. 
+    Initially focused at apache logs
+    """
     # TODO: this is not being GC properly, causing a memory leak.... sloppy...
     assert r is not None
     assert totalstatus is not None
@@ -37,11 +44,62 @@ def add_entry(r=None,totalstatus=None):
 
 
 
-def uno():
-    pass
+################################################################################
+#  Splitted methods to use with add entry
+################################################################################
 
-def dos():
-    pass
+def get_total_count(r=None, totalstatus=None):
+    """
+    get the total count of hits that returned an error code
+    """
+    assert r is not None
+    assert totalstatus is not None
+    try:
+        totalstatus[str(r['status'])]['count'] += 1
+    except KeyError:
 
-def tres():
-    pass
+        totalstatus[str(r['status'])] = {}
+        totalstatus[str(r['status'])]['count'] = 1
+    return totalstatus
+
+
+def get_uri(r=None, totalstatus=None):
+    """add the request that caused the code"""
+    assert r is not None
+    assert totalstatus is not None
+    try:
+        totalstatus[str(r['status'])]["URI"][r['request']] += 1
+    except KeyError:
+        # if it's not being found till now, create a new entry in stats
+        try:
+            if totalstatus[str(r['status'])]["URI"]:
+                totalstatus[str(r['status'])]["URI"][r['request']] = 1
+        except:
+            totalstatus[str(r['status'])]["URI"] = {}
+            totalstatus[str(r['status'])]["URI"][r['request']] = 1
+    return totalstatus
+
+
+def get_ip(r=None, totalstatus=None):
+    """ add the IP count"""
+    assert r is not None
+    assert totalstatus is not None
+    try:
+        totalstatus[str(r['status'])][r['host']] += 1
+    except KeyError:
+        totalstatus[str(r['status'])][r['host']] = 1
+
+
+def get_useragent(r=None, totalstatus=None):
+    """ add the user agent"""
+    assert r is not None
+    assert totalstatus is not None
+    try:
+        totalstatus[str(r['status'])]["useragents"][r['useragent']] += 1
+    except KeyError:
+        # if it's not being found till now, create a new entry in stats
+        totalstatus[str(r['status'])]["useragents"] = {}
+        totalstatus[str(r['status'])]["useragents"][r['useragent']] = 1
+         
+
+
