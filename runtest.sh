d81ecd7e0f5feb6a7c93a0f64939f259f704f292
Juan Morales <jmorales@log01.uswest2.siteminder.com>
2014-12-01 17:57:32 +0000

    logging issue still unfixed

--- runtest.sh
+++ runtest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "----------- apache no kind"
-./greap -i ../crap/apache-onweb/access-vpn.fidelio.log  -q host,referrer,user,datetime,method,request,proto,status,bytes,from,useragent  | tail -n 5
+./greap -i sample.apache.log  -q host,referrer,user,datetime,method,request,proto,status,bytes,from,useragent  | tail -n 5
 
 echo "----------- apache no query"
-./greap -i ../crap/apache-onweb/access-vpn.fidelio.log  | tail -n 5
+./greap -i sample.apache.log  | tail -n 5
 
 echo "----------- channel manager no kind"
 ./greap -i sample.app.log | tail -n 5
@@ -14,3 +14,9 @@ echo "----------- channel manager w kind  no query"
 
 echo "----------- channel manager w kind w query"
 ./greap -i sample.app.log -k channel_manager -q loglevel,action,jobtype | tail -n 5
+
+echo "----------- channel breaker w kind  no query"
+./greap -i sample.ap2.log -k channel_manager | tail -n 5
+
+echo "----------- channel breaket w kind w query"
+./greap -i sample.ap2.log -k channel_manager -q loglevel,action,jobtype | tail -n 5
