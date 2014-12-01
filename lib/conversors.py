57da615f5cf23f377205cd5a3295ae4dcbdefc51
Juan Morales <jmorales@log01.uswest2.siteminder.com>
2014-12-01 17:57:32 +0000

    logging issue still unfixed

--- lib/conversors.py
+++ lib/conversors.py
@@ -1,6 +1,7 @@
 
 import time
 import xml.dom.minidom as mdom
+import xml.dom.expatbuilder as expatbuilder
 import re
 
 from helpers import output
@@ -22,14 +23,16 @@ def convert_time(strtime, in_format = "%d/%b/%Y:%H:%M:%S +0000", out_format = "%
 def clean_action_to_xml(action):
     """
     takes the string corresponding to an "action" in a channel manager 
-    log, and extracts the XML and the kind of message (request or response)
+    log, and extracts the XML and the kind of message (request or response).
+    In case of error, it should raise an exception and the caller will
+    return the original action then
     """
     expresion = r"\[(.*)\] \[(.*)\]"
     reg = re.compile(expresion)
     try:
         fields = reg.match(action).groups()
     except:
-        raise TypeError("This doesn't look like a request/response")
+        raise TypeError("This doesn't look like a request/response","DEBUG")
     return fields
 
 
@@ -46,7 +49,7 @@ def convert_xml(content=None, root = "root"):
         #xml = mdom.parseString('<root>'+cleaned+'</root>')
         xml = mdom.parseString('<{0}>{1}</{0}>'.format(root,cleaned))
         return xml.toprettyxml()
-    except Exception as e:
+    except (TypeError,expatbuilder.expat.ExpatError) as e:
         output("Couldnt parse XML, {0}".format(e.__str__()), "DEBUG")
         return content
 
