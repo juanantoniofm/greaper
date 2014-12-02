d368a2e1db9338e7245d34a819090fb4292d10c0
Juan Morales <juan.morales@L0147.local>
2014-12-02 09:02:44 +0000

    renamed app_log to channel_manager_log

--- runtest.sh
+++ runtest.sh
@@ -20,3 +20,6 @@ echo "----------- channel breaker w kind  no query"
 
 echo "----------- channel breaket w kind w query"
 ./greap -i sample.ap2.log -k channel_manager -q loglevel,action,jobtype | tail -n 5
+=======
+echo "----------- channel manager w kind w whole-query"
+./greap -i sample.app.log -k channel_manager -q logdate,machine,logfile,datetime,loglevel,tracing,jobtype,action | tail -n 5
